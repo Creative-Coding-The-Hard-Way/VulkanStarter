0f445397f6bb958046ddc61929f9f2974bab4d9b
Bradley Lyman <lyman.brad3211@gmail.com>
2021-01-23 18:23:23 -0700

    setup application structure
    because:
    - it'll be nice to have a starting point for more development
    - most examples online don't show much structure, instead favoring
      a single giant main() method, and I want something I can grow
      in a more healthy way

--- src/application.rs
+++ src/application.rs
@@ -1,11 +1,63 @@
-pub struct Application {}
+use std::error::Error;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::{Window, WindowBuilder};
+
+pub struct Application {
+    event_loop: Option<EventLoop<()>>,
+    window: Window,
+}
 
 impl Application {
-    pub fn initialize() -> Application {
-        Application {}
+    pub fn initialize() -> Result<Application, Box<dyn Error>> {
+        let event_loop: EventLoop<()> = EventLoop::new();
+        let window: Window = WindowBuilder::new()
+            .with_title("vulkan experiments")
+            .with_resizable(false)
+            .with_decorations(true)
+            .with_visible(true)
+            .with_inner_size(LogicalSize::new(1366, 768))
+            .build(&event_loop)?;
+
+        Ok(Application {
+            event_loop: Option::Some(event_loop),
+            window,
+        })
     }
 
-    pub fn main_loop(&self) {
-        println!("nothing doing");
+    /**
+     * Render the screen.
+     */
+    fn render(&self) {
+        self.window.request_redraw();
+    }
+
+    /**
+     * Main application loop for this window. Blocks the thread until the
+     * window is closed.
+     */
+    pub fn main_loop(mut self) {
+        let event_loop = self.event_loop.take().unwrap();
+
+        event_loop.run(move |event, _, control_flow| {
+            *control_flow = ControlFlow::Poll;
+
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => {
+                    *control_flow = ControlFlow::Exit;
+                }
+
+                Event::MainEventsCleared => {
+                    // redraw here
+                    self.render();
+                }
+
+                _ => (),
+            }
+        });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+extern crate vulkano;
+extern crate winit;
+
 mod application;
 
 use application::Application;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let app = Application::initialize();
+    let app = Application::initialize()?;
     app.main_loop();
     Ok(())
 }
